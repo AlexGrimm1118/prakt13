da32be32301d573922ca6f7ba3909d061c319c80
AlexGrimm1118 <@sanysemenov1977gmail.com>
2022-06-02 22:33:27 +0500

    Start

---
+++ Consoleprakt13/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleprakt13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            while (true)
+            {
+                Console.WriteLine("\n1-Ввести А\n2-Ввести В\n3-Вычесть\n4-Сложить\n5-Умножить\n6-Делить");
+                int ch = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+
+
+
+        }
+    }
+}
